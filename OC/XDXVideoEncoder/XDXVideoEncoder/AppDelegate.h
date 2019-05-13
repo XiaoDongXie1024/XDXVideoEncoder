2e44d4180d92835c488fc64768a10d6d77b1688b
Demon <demonli@tvunetworks.com>
2019-05-13 12:21:11 +0800

    Init Project

---
+++ OC/XDXVideoEncoder/XDXVideoEncoder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XDXVideoEncoder
+//
+//  Created by 小东邪 on 2019/5/13.
+//  Copyright © 2019 小东邪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OC/XDXVideoEncoder/XDXVideoEncoder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XDXVideoEncoder
+//
+//  Created by 小东邪 on 2019/5/13.
+//  Copyright © 2019 小东邪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
